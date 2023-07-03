6437fbdac485b3a096d35d107c16bfbe76243e3d
Tory Adams <tory@vikingson.xyz>
2023-07-03 11:52:52 -0700

    types added and converted to typescript

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,22 @@
 import fetch from 'node-fetch';
 
+type tokens = {
+    access_token: string;
+}
+
 export default class Auth0 {
-    client_id; 
-    domain;
-    client_secret;
-    audience; 
-    constructor(client_id, domain, client_secret, audience) {
+    private client_id: string; 
+    private domain: string;
+    private client_secret: string;
+    private audience: string; 
+    constructor(client_id: string, domain: string, client_secret: string, audience: string) {
         this.client_id = client_id;
         this.domain = domain;
         this.client_secret = client_secret;
         this.audience = audience;
     }
 
-    async getAccessToken() {
+    async getAccessToken(): Promise<string> {
         const response = await fetch(`https://${this.domain}/oauth/token`, {
             method: 'POST',
             headers: { 'content-type': 'application/json' },
@@ -23,8 +27,12 @@ export default class Auth0 {
                 grant_type: 'client_credentials'
             })
         }); 
-        console.log((await response.blob()).toString());
-        const responseJson = await response.json();
+
+        if (response.status !== 200) {
+            throw new Error('Unable to fetch access token');
+        }
+
+        const responseJson: tokens = await response.json() as tokens;
         return responseJson.access_token;
     }
 }
